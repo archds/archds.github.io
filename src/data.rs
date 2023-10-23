5aaf49a0376ad85d942a4fb18bd8c1bd06711eb1
archds <dsalekseev11@gmail.com>
2023-10-24 00:56:48 +0700

    Local assets

--- src/data.rs
+++ src/data.rs
@@ -1,6 +1,6 @@
 use crate::types::ApplicationData;
 
-const DATA_RAW: &str = include_str!("data.json");
+const DATA_RAW: &str = include_str!("assets/data.json");
 
 pub fn get_data() -> Result<ApplicationData, serde_json::Error> {
     serde_json::from_str::<ApplicationData>(DATA_RAW)
