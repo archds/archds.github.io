a47c1b02de39f52e1c4c1b036fbd63bba5ec7478
archds <76770610+archds@users.noreply.github.com>
2023-10-24 17:47:23 +0000

    Deploying to gh-pages from @ archds/archds.github.io@16be63b02fd0e992840a652993b35fb5968017b2 🚀

--- trunk-template-fa08c5b83198a2d7.js
+++ trunk-template-fa08c5b83198a2d7.js
@@ -584,12 +584,12 @@ function __wbg_get_imports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper690 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 288, __wbg_adapter_16);
+    imports.wbg.__wbindgen_closure_wrapper712 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 306, __wbg_adapter_16);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper869 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 358, __wbg_adapter_19);
+    imports.wbg.__wbindgen_closure_wrapper891 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 376, __wbg_adapter_19);
         return addHeapObject(ret);
     };
 
@@ -631,7 +631,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('trunk-template-852c24f89553c5fc_bg.wasm', import.meta.url);
+        input = new URL('trunk-template-fa08c5b83198a2d7_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
