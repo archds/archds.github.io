3d2a4de5ca64a9633b87c49e4000eedcfe37f7e6
archds <dsalekseev11@gmail.com>
2023-10-25 00:46:59 +0700

    Change col size

--- src/components/made.rs
+++ src/components/made.rs
@@ -9,8 +9,8 @@ pub fn MadeWith(props: &MadeWithProps) -> Html {
     let MadeWithProps {} = props;
     html! {
         <layout::Row>
-            <layout::Column size={"9"}><div></div></layout::Column>
-            <layout::Column size={"3"}>
+            <layout::Column size={"8"}><div></div></layout::Column>
+            <layout::Column size={"4"}>
                 <span class={classes!("text-grey")}>{"Made with: "}</span>
                 <a target={"_blank"} href={"https://yew.rs/"}>{"Yew"}</a><span>{", "}</span>
                 <a target={"_blank"} href={"https://jenil.github.io/chota/"}>{"chota"}</a><span>{", "}</span>
